d33bf5d484db7a51391be304eae4211c1950ab81
thiago <thiagoprocaci@gmail.com>
2019-02-17 13:02:37 -0300

    @RequestParam

--- src/main/java/com/tbp/controller/HobbyController.java
+++ src/main/java/com/tbp/controller/HobbyController.java
@@ -3,10 +3,10 @@ package com.tbp.controller;
 import com.tbp.model.Hobby;
 import com.tbp.repository.HobbyRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.Map;
 
--- src/main/java/com/tbp/controller/PersonController.java
+++ src/main/java/com/tbp/controller/PersonController.java
@@ -5,10 +5,10 @@ import com.tbp.model.Person;
 import com.tbp.repository.HobbyRepository;
 import com.tbp.repository.PersonRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.Map;
 
