c114b97bc1f2dba70675cb60777048c41426a787
thiago <thiagoprocaci@gmail.com>
2019-02-20 06:50:20 -0300

    preparendo o template

--- src/main/java/com/tbp/controller/HobbyController.java
+++ src/main/java/com/tbp/controller/HobbyController.java
@@ -6,35 +6,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 
 import org.springframework.web.bind.annotation.*;
 
-@RestController
-@RequestMapping("hobby")
+
 public class HobbyController {
 
-    @Autowired
-    HobbyRepository hobbyRepository;
-
-    @RequestMapping(method = RequestMethod.GET)
-    public Iterable<Hobby> findAll() {
-        return hobbyRepository.findAll();
-    }
-
-    @RequestMapping(method = RequestMethod.POST)
-    public Hobby save(@RequestBody Hobby hobby) {
-        Hobby savedHobby = hobbyRepository.save(hobby);
-        return savedHobby;
-    }
-
-    @RequestMapping(method = RequestMethod.PUT)
-    public Hobby update(@RequestBody Hobby hobby) {
-        Hobby savedHobby = hobbyRepository.save(hobby);
-        return savedHobby;
-    }
-
-
-    @RequestMapping(method = RequestMethod.DELETE)
-    public void remove(@RequestBody Hobby hobby) {
-        hobbyRepository.delete(hobby.getId());
-    }
 
 
 }
--- src/main/java/com/tbp/controller/PersonController.java
+++ src/main/java/com/tbp/controller/PersonController.java
@@ -8,36 +8,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 
-@RestController
-@RequestMapping("person")
+
 public class PersonController {
 
-    @Autowired
-    PersonRepository personRepository;
-    @Autowired
-    HobbyRepository hobbyRepository;
 
-
-    @RequestMapping(method = RequestMethod.GET)
-    public Iterable<Person>  findAll() {
-        Iterable<Person> all = personRepository.findAll();
-        return all;
-    }
-
-    @RequestMapping(method = RequestMethod.POST)
-    public Person save(@RequestBody Person person) {
-        Person p = personRepository.save(person);
-        return p;
-    }
-
-    @RequestMapping(method = RequestMethod.PUT)
-    public Person update(@RequestBody Person person) {
-       Person p = personRepository.save(person);
-       return p;
-    }
-
-    @RequestMapping(method = RequestMethod.DELETE)
-    public void remove(@RequestBody Person person) {
-        personRepository.delete(person.getId());
-    }
 }
