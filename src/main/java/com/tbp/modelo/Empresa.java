5c544020340228b63555786fbae31eb249dc7e44
thiago <thiagoprocaci@gmail.com>
2021-03-18 20:14:16 -0300

    versao desktop

---
+++ src/main/java/com/tbp/modelo/Empresa.java
@@ -0,0 +1,63 @@
+package com.tbp.modelo;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+
+@Entity
+@Table(name = "empresa")
+public class Empresa {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    @Column(name = "nome")
+    private String nome;
+    @Column(name = "cnpj")
+    private String cnpj;
+
+    public Empresa(String nome, String cnpj) {
+        this.nome = nome;
+        this.cnpj = cnpj;
+    }
+    
+    public Empresa() {
+        
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCnpj() {
+        return cnpj;
+    }
+
+    public void setCnpj(String cnpj) {
+        this.cnpj = cnpj;
+    }
+
+    @Override
+    public String toString() {
+        return nome ;
+    }
+    
+    
+    
+}
---
+++ src/main/java/com/tbp/repository/FuncionarioRepository.java
@@ -0,0 +1,9 @@
+
+package com.tbp.repository;
+
+import com.tbp.modelo.Funcionario;
+import org.springframework.data.repository.CrudRepository;
+
+public interface FuncionarioRepository extends CrudRepository<Funcionario, Integer>{
+    
+}
