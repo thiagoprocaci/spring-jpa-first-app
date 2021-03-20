a1939ba7880ceccb5d62734651a5b652d8c2ce01
thiago <thiagoprocaci@gmail.com>
2021-03-20 12:57:34 -0300

    preparação template

--- src/main/java/com/tbp/repository/EmpresaRepository.java
+++ src/main/java/com/tbp/repository/EmpresaRepository.java
@@ -4,7 +4,6 @@ import com.tbp.modelo.Empresa;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
-@Repository
-public interface EmpresaRepository extends CrudRepository<Empresa, Integer>{
+public interface EmpresaRepository {
     
 }
--- src/main/java/com/tbp/repository/FuncionarioRepository.java
+++ src/main/java/com/tbp/repository/FuncionarioRepository.java
@@ -5,8 +5,6 @@ import com.tbp.modelo.Funcionario;
 import java.util.List;
 import org.springframework.data.repository.CrudRepository;
 
-public interface FuncionarioRepository extends CrudRepository<Funcionario, Integer>{
-    
-    List<Funcionario> findByNomeIgnoreCaseContaining(String nome);
+public interface FuncionarioRepository {
     
 }
