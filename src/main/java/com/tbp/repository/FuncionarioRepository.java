8b39282fd24ed6e2e447e42cab03754333ff4823
thiago <thiagoprocaci@gmail.com>
2021-04-07 21:50:38 -0300

    Teste 2

--- src/main/java/com/tbp/repository/FuncionarioRepository.java
+++ src/main/java/com/tbp/repository/FuncionarioRepository.java
@@ -5,6 +5,6 @@ import com.tbp.modelo.Funcionario;
 import java.util.List;
 import org.springframework.data.repository.CrudRepository;
 
-public interface FuncionarioRepository  {
+public interface FuncionarioRepository {
     
 }
